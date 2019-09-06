278b4190109e106982e86497737e5980fb2c2c6c
Ellen Saylor <ellensaylor8@gmail.com>
2019-09-06 15:45:25 -0500

    set up get router

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /** ---------- ROUTES ---------- **/
 const movieRouter = require('./routes/movieRouter');
