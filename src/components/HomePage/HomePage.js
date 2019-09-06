0a103d8475db9b8f1754d9de1c74703ccecd7dc3
Ellen Saylor <ellensaylor8@gmail.com>
2019-09-06 15:52:04 -0500

    set up standard HomePage component

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React, { component } from 'react';
+
+class HomePage extends component {
+    render() {
+        return (
+            <div>
+                <h1>HOME PAGE</h1>
+            </div>
+        );
+    }
+
+}
+
+export default HomePage;
