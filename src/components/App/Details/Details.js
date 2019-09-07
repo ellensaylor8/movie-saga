0931fcb7db23243a959d8e7531583a1860292f65
Ellen Saylor <ellensaylor8@gmail.com>
2019-09-07 10:42:55 -0500

    set up back button on details page to return to home page

--- src/components/App/Details/Details.js
+++ src/components/App/Details/Details.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button'
+import { withRouter } from 'react-router-dom';
 
+function Details(props) {
+    const movieInfo = props.reduxState.movies.filter((movie, index) => {
+        console.log(movie, 'haaa!');
 
-// function Details(props) {
-function Details(props){
-        const movieInfo = props.reduxState.movies.filter((movie, index) => {
-            console.log(movie, 'haaa!');
+        return movie.id == props.match.params.id
 
-            return movie.id == props.match.params.id
+    })
 
-        })
-        console.log('movieInfo: ',movieInfo)
-        return (
-            <div>
-                <h1>Details</h1>
-                <p>Title: <i>{movieInfo[0].title}</i></p>
-                <p>Genre: <i>{movieInfo[0].name}</i></p>
-                <p>Description: <i>{movieInfo[0].description}</i></p>
-            </div>
-        );
+    const returnToHome = () => {
+        props.history.push('/')
     }
 
+    return (
+        <div>
+            <h1>Details</h1>
+            <Button onClick={returnToHome}>Back</Button>
+            <Button>Edit</Button>
+            <p>Title: <i>{movieInfo[0].title}</i></p>
+            <p>Genre: <i>{movieInfo[0].name}</i></p>
+            <p>Description: <i>{movieInfo[0].description}</i></p>
+        </div>
+    );
+}
 
 const mapStateToProps = reduxState => ({
     reduxState,
 });
 
-export default connect(mapStateToProps)(Details);
+export default connect(mapStateToProps)(withRouter(Details));
